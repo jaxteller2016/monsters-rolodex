6f7c845a391a61ffb33b8a83a119237af7459fbb
jaxteller2016 <sorin.craciunescu84@gmail.com>
2020-05-01 18:58:03 +0300

    Updates

---
+++ precache-manifest.c7fd17cfec7b10eec9ef4fab0905babe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e70e7d83c4951862fdc200ba062729b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a655d49e4becf710a22e",
+    "url": "/monsters-rolodex/static/css/main.958128af.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a655d49e4becf710a22e",
+    "url": "/monsters-rolodex/static/js/main.ec12f526.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
